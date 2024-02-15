3e8ee0900f5e3e7b3424be29ba2daae9e044ee70
vovanolo <vpankiv@softserveinc.com>
2024-02-15 13:04:23 +0200

    add: folder app for routes: / - index.js

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text} from 'react-native';
+
+import { Link } from 'expo-router';
+
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Main page</Text>
+    </View>
+  );
+}
