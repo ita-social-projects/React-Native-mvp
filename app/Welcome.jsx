fff8c4441ec434abee6367e231ebe23c2fdad1a0
vovanolo <vpankiv@softserveinc.com>
2024-02-22 12:21:59 +0200

    add: Home, Welcome pages

---
+++ app/Welcome.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class Welcome extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Welcome</Text>
+      </View>
+    )
+  }
+}
