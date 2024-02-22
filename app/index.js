940d7a98b1ca36e739c68e5cebb436084aa368b1
vovanolo <vpankiv@softserveinc.com>
2024-02-22 12:11:28 +0200

    add: localization

--- app/index.js
+++ app/index.js
@@ -1,12 +1,23 @@
-import { View, Text} from 'react-native';
+import { View, Text, Button} from 'react-native';
+
+import React, { useState } from "react"
 
 import { Link } from 'expo-router';
 
+import * as Localization from 'expo-localization';
+import { I18n } from 'i18n-js'
+import { translations } from '../localization/localization.js';
+
 
 export default function Home() {
+  let [locale, setLocale] = useState(Localization.locale)
+  const i18n = new I18n(translations)
+
   return (
     <View>
-      <Text>Main page</Text>
+      <Text>{i18n.t('greeting')}</Text>
+      <Button onPress={() => setLocale("en")} title="English" color="#841584" />
+      <Button onPress={() => setLocale("uk")} title="Ukrainian" color="#841584" />
     </View>
   );
 }
