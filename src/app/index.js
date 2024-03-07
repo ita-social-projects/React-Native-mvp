0906182c2e3831df29a871309b00db1cea1afd81
Anatoliy Sarakhman <anatolej1111@gmail.com>
2024-03-07 17:51:48 +0200

    added import aliases

---
+++ src/app/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { View, Text, Button } from "react-native";
+import * as Localization from "expo-localization";
+import { Link } from "expo-router";
+import { I18n } from "i18n-js";
+
+import { translations } from "~/localization/localization";
+
+const Home = () => {
+  let [locale, setLocale] = useState(Localization.locale);
+  const i18n = new I18n(translations);
+
+  return (
+    <View>
+      <Text>{i18n.t("greeting")}</Text>
+      <Button onPress={() => setLocale("en")} title='English' color='#841584' />
+      <Button
+        onPress={() => setLocale("uk")}
+        title='Ukrainian'
+        color='#841584'
+      />
+    </View>
+  );
+}
+
+export default Home
